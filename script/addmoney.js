b369a61d7f56df158073e770206dd71605eb60c7
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-02-22 22:59:01 +0600

    history added to transaction section

---
+++ script/addmoney.js
@@ -0,0 +1,49 @@
+document.getElementById("addMoney_btn")
+    .addEventListener("click", function () {
+        // select add money bank
+        const bankAccount = getValueFromInput("add_money_bank");
+        if (bankAccount === "Select a Bank") {
+            alert("Please Select a Bank");
+            return;
+        }
+
+        // get bank account number 
+        const bankAccountNumber = getValueFromInput("bank_account_number")
+        if (bankAccountNumber.length !== 11) {
+            alert("Invalid bank number");
+            return;
+        }
+
+        // get add money 
+        const addMoney = getValueFromInput("amount_add");
+        const currentBalance = getBalance();
+
+        const newBalance = currentBalance + Number(addMoney);
+
+        // pin validated
+        const pin = getValueFromInput("bankPin_number");
+
+        if (pin === "1234") {
+            alert(`Add money successfully from ${bankAccount} at ${new Date()}`)
+            setBalance(newBalance);
+
+            const history = document.getElementById("history_container");
+
+            const newHistory = document.createElement("div");
+
+            newHistory.innerHTML= `
+              <div class="transaction_card p-5 bg-base-100">
+                Add money successfully from ${bankAccount},acc-no ${bankAccountNumber} at ${new Date()}
+              </div>
+            `
+
+            history.appendChild(newHistory)
+
+
+        }
+        else {
+            alert("Invalid Pin");
+            return;
+        }
+
+    })
--- script/machine.js
+++ script/machine.js
@@ -18,3 +18,21 @@ function setBalance(value){
     const balanceElement = document.getElementById("currentBalance");
     balanceElement.innerText = value;
 }
+
+// machine id > hide all > show id
+
+function showOnly(id){
+    const addMoney = document.getElementById("add_money");
+    const cashOut = document.getElementById("cashOut");
+    const sendMoney = document.getElementById("send_money")
+    const payBill = document.getElementById("paybill")
+    
+
+    addMoney.classList.add("hidden")
+    cashOut.classList.add("hidden")
+    sendMoney.classList.add("hidden")
+    payBill.classList.add("hidden")
+
+    const selected = document.getElementById(id);
+    selected.classList.remove("hidden")
+}
